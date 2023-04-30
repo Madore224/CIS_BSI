bfaed2b7e695e36f39cf03566542b20abc5a7a37
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-04-30 15:42:23 +0200

    Update main_global.sh

--- global/main_global.sh
+++ global/main_global.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+echo "Execution script global"
+
+# Chemin vers le dossier contenant les scripts de niveau 2
+script_level2="./level2"
+
+for fichierLv2 in $script_level2/*; do
+  # Vérifier si le fichier est un script exécutable
+  if [[ -x "$fichierLv2" && -f "$fichierLv2" ]]; then
+    # Exécuter le script
+    echo "$fichierLv2"
+    bash "$fichierLv2"
+  fi
+done
