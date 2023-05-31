610bbb773c1cdc8e1435d923dc89a0789da41b02
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-01 00:25:21 +0200

    Add files via upload

---
+++ server/level1/cups_confLv2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Fiable 98 %
+
+echo -e "START disabled autofs...\n"
+
+if dpkg -s cups >/dev/null 2>&1; then
+	echo "Cups is installed."
+	while true; do
+	
+		echo "Votre système aura-t-il besoin d'imprimer des travaux ? (yes/no)"
+		
+		read choix
+		
+		if [ "$choix" = "no" ]; then
+			echo "You choose to delete Cups..."
+			sudo apt purge cups
+			echo "Cups now is delete."
+			break
+		elif [ "$choix" = "yes" ]; then
+			echo "Cups is safe."
+			break
+		else
+			echo "Invalid choose"
+		fi
+	done
+else
+	echo "Cups is not installed."
+fi
