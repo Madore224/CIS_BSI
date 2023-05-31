8cf41d7d86e29eaeadd96815ab1b00aa255648dd
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-01 00:28:49 +0200

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Bonjour ! "
+
+# Vérifier si l'utilisateur a les privilèges sudo
+if sudo -n true 2>/dev/null; then
+	echo "L'utilisateur a les privilèges sudo."
+	# chemin vers le script à exécuter
+	script_global="global/main_global.sh"
+	script_workstation="workstation/main_workstation.sh"
+	script_server="server/main_server.sh"
+
+	echo "Veuillez choisir une option d'installation (écrire 1 ou 2) :"
+	echo "	1. Installation serveur"
+	echo "	2. Installation workstation"
+	read choix
+
+	# Vérifier la réponse de l'utilisateur
+	if [[ $choix == "1" ]]; then
+		echo "Vous avez choisi l'option A."
+	elif [[ $choix == "2" ]]; then
+	 	# vérifier si le fichier existe
+		if [ -f $script_global ]; then
+		  # exécuter le script main_global
+		  bash $script_global
+		  
+		  if [ -f $script_workstation ]; then
+		  	# exécuter le script main_workstation
+		  	bash $script_workstation
+		  else
+		  	echo "Erreur: le fichier $script_workstation n'existe pas"
+		  fi
+		  
+		else
+		  echo "Erreur: le fichier $script_global n'existe pas"
+		fi
+	else
+	 	echo "Choix invalide. Veuillez choisir entre l'option 1 ou l'option 2."
+	fi
+else
+    echo "L'utilisateur n'a pas les privilèges sudo."
+    echo "Exécuter la commande 'sudo su' pour passer root puis relancer le programme."
+fi
+
