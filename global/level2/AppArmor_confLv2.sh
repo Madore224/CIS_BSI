fb73a52805d4ae2985ebd530968c289da988d392
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-09 17:26:25 +0200

    Update AppArmor_confLv2.sh

--- global/level2/AppArmor_confLv2.sh
+++ global/level2/AppArmor_confLv2.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo -e "START AppArmor set profiles...\n"
-
-sudo apt-get install apparmor-utils
-echo "install module apparmor-utils"
-sudo aa-enforce /etc/apparmor.d/*
-
-
+if dpkg -s apparmor-utils >/dev/null 2>&1; then
+	echo "[+] Apparmor-utils est déjà installé."
+else
+    	apt-get install -y "$package" >/dev/null 2>&1
+    	if [ $? -eq 0 ]; then
+		echo "[+] Package $package installé avec succès."
+		aa-enforce /etc/apparmor.d/*
+	else
+	    echo "[+] Erreur lors de l'installation du package $package."
+	fi	
+fi
