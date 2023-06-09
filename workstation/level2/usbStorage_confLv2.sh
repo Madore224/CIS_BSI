b95afc0aca8f55de3f460f35bdd600d3a7297616
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-09 17:34:28 +0200

    Update usbStorage_confLv2.sh

--- workstation/level2/usbStorage_confLv2.sh
+++ workstation/level2/usbStorage_confLv2.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 #Fiable 65 %
-echo -e "START unload the usb-storage...\n"
+echo "[+] Unload the usb-storage..."
 
-if [ -e /etc/modprobe.d/usb_storage.conf ]; then
-    echo "File exist"
-    echo "install usb-storage /bin/true" >> /etc/modprobe.d/usb_storage.conf
-else
-    echo -e "File not exist"
-    touch /etc/modprobe.d/usb_storage.conf
-    echo "install usb-storage /bin/true" >> /etc/modprobe.d/usb_storage.conf
+output1=$(modprobe -n -v usb-storage)
+output2=$(lsmod | grep usb-storage)
+
+
+if [[ "$output1" == "install /bin/true" ]]; then
+	if [ -z "$output2" ]; then
+		if [ -e /etc/modprobe.d/usb_storage.conf ]; then
+			echo "install usb-storage /bin/true" >> /etc/modprobe.d/usb_storage.conf
+		else
+			touch /etc/modprobe.d/usb_storage.conf
+		    	echo "install usb-storage /bin/true" >> /etc/modprobe.d/usb_storage.conf
+		    	rmmod usb-storage
+		fi
+	fi
 fi
 
-rmmod usb-storage
+
