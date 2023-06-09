a0ed031b6d49ba44df1e556aaab470c051022f8f
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-09 17:26:06 +0200

    Update disable_ipv6.sh

--- global/level2/disable_ipv6.sh
+++ global/level2/disable_ipv6.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo -e "Disable IPV6"
+echo "[+] Disable IPV6..."
 
-sed -i '/GRUB_CMDLINE_LINUX="/d' /etc/default/grub
+if ! grep "^\s*linux" /boot/grub/grub.cfg | grep -v "ipv6.disable=1" >/dev/null; then
+	echo "[+]"
+else
+    	sed -i '/GRUB_CMDLINE_LINUX="/d' /etc/default/grub
 
-echo 'GRUB_CMDLINE_LINUX="ipv6.disable=1 find_preseed=/preseed.cfg auto no prompt priority=critical locale=en_US"' >> /etc/default/grub
+	echo 'GRUB_CMDLINE_LINUX="ipv6.disable=1 find_preseed=/preseed.cfg auto no prompt priority=critical locale=en_US"' >> /etc/default/grub
 
-update-grub
+	update-grub >/dev/null 2>&1
+fi
