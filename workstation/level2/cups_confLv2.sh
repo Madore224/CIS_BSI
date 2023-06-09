8bac9a8f2cc7d96ca46451b768b91e03865d074d
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-09 17:29:24 +0200

    Update cups_confLv2.sh

--- workstation/level2/cups_confLv2.sh
+++ workstation/level2/cups_confLv2.sh
@@ -2,7 +2,7 @@
 
 #Fiable 98 %
 
-echo -e "START disabled autofs...\n"
+echo "[+] Delete Cups..."
 
 if dpkg -s cups >/dev/null 2>&1; then
 	echo "Cups is installed."
@@ -24,6 +24,4 @@ if dpkg -s cups >/dev/null 2>&1; then
 			echo "Invalid choose"
 		fi
 	done
-else
-	echo "Cups is not installed."
 fi
