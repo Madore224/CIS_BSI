f1aa7410d4b3b45423947508fc8ef76b2e2761c9
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-11 13:11:16 +0200

    Update 70_usbStorage.sh

--- workstation/level2/70_usbStorage.sh
+++ workstation/level2/70_usbStorage.sh
@@ -11,6 +11,7 @@ if [[ "$output1" == "install /bin/true" ]]; then
 	if [ -z "$output2" ]; then
 		if [ -e /etc/modprobe.d/usb_storage.conf ]; then
 			echo "install usb-storage /bin/true" >> /etc/modprobe.d/usb_storage.conf
+			rmmod usb-storage
 		else
 			touch /etc/modprobe.d/usb_storage.conf
 		    	echo "install usb-storage /bin/true" >> /etc/modprobe.d/usb_storage.conf
