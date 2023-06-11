be01655e0defbf0405a4787858eb7984ba566e70
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-11 15:50:24 +0200

    Create 207_disable_DCCP.sh

---
+++ global/level2/207_disable_DCCP.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "[+] Unload the usb-storage..."
+
+output2=$(lsmod | grep dccp)
+
+check="false"
+if modprobe -n -v dccp | grep -q "install /bin/true"; then
+	if [ -z "$output2" ]; then
+		check="true"	
+	fi
+fi
+
+if [[ "$check" == "false" ]]; then
+	if [ -e /etc/modprobe.d/dccp.conf ]; then
+		echo "install dccp /bin/true" >> /etc/modprobe.d/dccp.conf
+
+	else
+		touch /etc/modprobe.d/dccp.conf
+		echo "install dccp /bin/true" >> /etc/modprobe.d/dccp.conf
+
+	fi
+fi
