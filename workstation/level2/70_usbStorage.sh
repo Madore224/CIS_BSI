86631c5cceb391c16e536d1e9bd76844bbbded16
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-11 13:56:49 +0200

    Update 70_usbStorage.sh

--- workstation/level2/70_usbStorage.sh
+++ workstation/level2/70_usbStorage.sh
@@ -6,17 +6,21 @@ echo "[+] Unload the usb-storage..."
 output1=$(modprobe -n -v usb-storage)
 output2=$(lsmod | grep usb-storage)
 
-
-if [[ "$output1" == "install /bin/true" ]]; then
+check="false"
+if modprobe -n -v usb-storage >/dev/null 2>&1 == "install /bin/true"; then
 	if [ -z "$output2" ]; then
-		if [ -e /etc/modprobe.d/usb_storage.conf ]; then
-			echo "install usb-storage /bin/true" >> /etc/modprobe.d/usb_storage.conf
-			rmmod usb-storage
-		else
-			touch /etc/modprobe.d/usb_storage.conf
-		    	echo "install usb-storage /bin/true" >> /etc/modprobe.d/usb_storage.conf
-		    	rmmod usb-storage
-		fi
+		check="true"	
+	fi
+fi
+
+if [[ "$check" == "false" ]]; then
+	if [ -e /etc/modprobe.d/usb_storage.conf ]; then
+		echo "install usb-storage /bin/true" >> /etc/modprobe.d/usb_storage.conf
+		rmmod usb-storage
+	else
+		touch /etc/modprobe.d/usb_storage.conf
+		echo "install usb-storage /bin/true" >> /etc/modprobe.d/usb_storage.conf
+		rmmod usb-storage
 	fi
 fi
 
