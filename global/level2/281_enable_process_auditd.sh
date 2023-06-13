bf833208f8aa2b611b477a686f75e7c44d61f9b8
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-13 12:03:48 +0200

    Update 281_enable_process_auditd.sh

--- global/level2/281_enable_process_auditd.sh
+++ global/level2/281_enable_process_auditd.sh
@@ -2,8 +2,6 @@
 
 echo "[+] Enable process auditd..."
 
-#!/bin/bash
-
 sed -i '/GRUB_CMDLINE_LINUX="/d' /etc/default/grub
 
 echo 'GRUB_CMDLINE_LINUX="ipv6.disable=1 audit=1 find_preseed=/preseed.cfg auto no prompt priority=critical locale=en_US"' >> /etc/default/grub
