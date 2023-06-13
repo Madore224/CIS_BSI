96396cbfd33b6a13650a0d28c389d5efcd81cc27
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-13 16:42:10 +0200

    Create 290_collect_logs_date_time_when_modify.sh

---
+++ global/level2/290_collect_logs_date_time_when_modify.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "[+] Collect logs when date or time are modify..."
+
+if [ -e "/etc/audit/rules.d/time-change.rules" ]; then
+    	echo "-a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change
+-a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change
+-a always,exit -F arch=b64 -S clock_settime -k time-change
+-a always,exit -F arch=b32 -S clock_settime -k time-change
+-w /etc/localtime -p wa -k time-change" > /etc/audit/rules.d/time-change.rules
+    	
+else
+	touch /etc/audit/rules.d/time-change.rules
+	echo "-a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change
+-a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change
+-a always,exit -F arch=b64 -S clock_settime -k time-change
+-a always,exit -F arch=b32 -S clock_settime -k time-change
+-w /etc/localtime -p wa -k time-change" >> /etc/audit/rules.d/time-change.rules
+fi
