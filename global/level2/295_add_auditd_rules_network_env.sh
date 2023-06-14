6b68a2c479d031c0a1fdf8402d1ae251d041c6ea
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-14 23:46:24 +0200

    Create 295_add_auditd_rules_network_env.sh

---
+++ global/level2/295_add_auditd_rules_network_env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "[+] Add auditd rules for collect logs of system network environment..."
+
+if [ -e "/etc/audit/rules.d/identity.rules" ]; then
+    	echo "-a always,exit -F arch=b64 -S sethostname -S setdomainname -k system-locale
+-a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale
+-w /etc/issue -p wa -k system-locale
+-w /etc/issue.net -p wa -k system-locale
+-w /etc/hosts -p wa -k system-locale
+-w /etc/network -p wa -k system-locale" > /etc/audit/rules.d/system-locale.rules
+    	
+else
+	touch /etc/audit/rules.d/system-locale.rules
+	echo "-a always,exit -F arch=b64 -S sethostname -S setdomainname -k system-locale
+-a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale
+-w /etc/issue -p wa -k system-locale
+-w /etc/issue.net -p wa -k system-locale
+-w /etc/hosts -p wa -k system-locale
+-w /etc/network -p wa -k system-locale" >> /etc/audit/rules.d/system-locale.rules
+fi
