fa960ae59aac82246f09add2630c32c162713105
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-15 22:12:08 +0200

    Create 308_add_auditd_rules_unsuccess_unauth_file_access_attempts.sh

---
+++ global/level2/308_add_auditd_rules_unsuccess_unauth_file_access_attempts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "[+] Add auditd rules for collect logs unsuccess unauthorize file access attempts..."
+
+if [ -e "/etc/audit/rules.d/access.rules" ]; then
+    	echo "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access
+-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access
+-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access
+-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access" > /etc/audit/rules.d/access.rules
+    	
+else
+	touch /etc/audit/rules.d/access.rules
+	echo "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access
+-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access
+-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access
+-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access" >> /etc/audit/rules.d/access.rules
+fi
