4a038530fe65832088484d779188cd2255948acc
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-18 21:45:01 +0200

    Update 404_ssh_allowTcpForwarding_disable.sh

--- global/level2/404_ssh_allowTcpForwarding_disable.sh
+++ global/level2/404_ssh_allowTcpForwarding_disable.sh
@@ -4,6 +4,4 @@ echo "[+] Disable ssh allow TCP Forwarding..."
 
 if [ -e "/etc/ssh/sshd_config" ]; then
 	echo "# AllowTcpForwarding no" >> /etc/ssh/sshd_config
-else
-	echo "File /etc/ssh/ssd_config not found."
 fi
