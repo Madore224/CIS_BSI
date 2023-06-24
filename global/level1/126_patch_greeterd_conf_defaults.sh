3b783f6adead4edbdd2adc1732995aeaafd30564
nnweg <119164122+nnweg@users.noreply.github.com>
2023-06-24 13:25:36 +0200

    Update 126_patch_greeterd_conf_defaults.sh

--- global/level1/126_patch_greeterd_conf_defaults.sh
+++ global/level1/126_patch_greeterd_conf_defaults.sh
@@ -9,6 +9,6 @@ echo -e "[+]$green Ensure GDM is removed or login is configured $endcolor"
 if [[ "$(dpkg-query -l | grep 'gdm3')" != "" ]]
 then
 	echo -e "[+]$green Patching /etc/gdm3/greeter.dconf-defaults $endcolor"
-	cp global/level1/patch_files/greeter.dconf-defaults /etc/gdm3/greeter.dconf-defaults > /dev/null 2&>1
+	cp global/level1/patch_files/greeter.dconf-defaults /etc/gdm3/greeter.dconf-defaults
 	dpkg-reconfigure gdm3 > /dev/null 2>&1
 fi
