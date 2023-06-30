dffd4aee0f5da65757a648e0266648486c5beac9
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-06-30 16:36:47 +0200

    Update main_global.sh for add choose the level of install

--- global/main_global.sh
+++ global/main_global.sh
@@ -9,7 +9,6 @@ red="\033[31m" #couleur texte
 endcolor="\033[0m" #fin couleur texte
 BAR="▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ ▰ " #progress bar
 
-nb_tour=0 #nombre de tour
 nb_scripts_lv1=$(($(ls -1 global/level1/*.sh | wc -l)*2)) #CHEMIN A MODIFIER (nombre de scripts)
 nb_scripts_lv2=$(($(ls -1 global/level2/*.sh | wc -l)*2)) #CHEMIN A MODIFIER (nombre de scripts)
 
@@ -17,35 +16,46 @@ nb_scripts_lv2=$(($(ls -1 global/level2/*.sh | wc -l)*2)) #CHEMIN A MODIFIER (no
 script_level1="global/level1" #(liste les scripts)
 script_level2="global/level2" #(liste les scripts)
 
+echo "1. Installation niveau 1"
+echo "2. Installation niveau 2"
+echo "3. Installation niveau 1 et 2"
 
-for fichierLv1 in "$script_level1"/*
-do
-  	# Vérifier si fichier existant
-	((nb_tour=nb_tour+2))
-	echo -ne "\r$green${BAR:0:$((nb_tour*20/nb_scripts_lv1))}$endcolor" $(( $nb_tour * 100/ $nb_scripts_lv1 ))"%\n"
-	if [ -f "$fichierLv1" ]; then
-		# Exécuter le script
-		bash "$fichierLv1"
-		sleep 5
-	else
-		echo "Erreur not file found"
-	fi
- 
-done
+echo -e "\nEntrez 1, 2 ou 3 :"
+read choix
 
 nb_tour=0
-for fichierLv2 in "$script_level2"/*
-do
-  	# Vérifier si fichier existant
-	((nb_tour=nb_tour+2))
-	echo -ne "\r$green${BAR:0:$((nb_tour*20/nb_scripts_lv2))}$endcolor" $(( $nb_tour * 100/ $nb_scripts_lv2 ))"%\n"
-	if [ -f "$fichierLv2" ]; then
-		# Exécuter le script
-		bash "$fichierLv2"
-		sleep 5
-	else
-		echo "Erreur not file found"
-	fi
-done
+if [[ $choix == "1" || $choix == "3" ]]; then
+	for fichierLv1 in "$script_level1"/*
+	do
+	  	# Vérifier si fichier existant
+		((nb_tour=nb_tour+2))
+		echo -ne "\r$green${BAR:0:$((nb_tour*20/nb_scripts_lv1))}$endcolor" $(( $nb_tour * 100/ $nb_scripts_lv1 ))"%\n"
+		if [ -f "$fichierLv1" ]; then
+			# Exécuter le script
+			bash "$fichierLv1"
+			sleep 5
+		else
+			echo "Erreur not file found"
+		fi
+	 
+	done
+ fi
+
+nb_tour=0
+if [[ $choix == "2" || $choix == "3" ]]; then
+	for fichierLv2 in "$script_level2"/*
+	do
+		# Vérifier si fichier existant
+		((nb_tour=nb_tour+2))
+		echo -ne "\r$green${BAR:0:$((nb_tour*20/nb_scripts_lv2))}$endcolor" $(( $nb_tour * 100/ $nb_scripts_lv2 ))"%\n"
+		if [ -f "$fichierLv2" ]; then
+			# Exécuter le script
+			bash "$fichierLv2"
+			sleep 5
+		else
+			echo "Erreur not file found"
+		fi
+	done
+fi
 
 /etc/init.d/auditd restart >/dev/null 2>&1
