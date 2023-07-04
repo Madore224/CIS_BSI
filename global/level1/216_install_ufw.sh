ea35ad1dbfb93c90bfca7df46482300fa3d15d0a
nnweg <119164122+nnweg@users.noreply.github.com>
2023-07-04 17:20:46 +0200

    Update 216_install_ufw.sh

--- global/level1/216_install_ufw.sh
+++ global/level1/216_install_ufw.sh
@@ -3,7 +3,7 @@
 
 #page 216
 echo -e "[+] Check if ufw is installed "
-if [ "$(dpkg -s ufw | grep 'not installed' > /dev/null 2>&1)" != "" ]
+if [ "$(dpkg-query -l | grep 'ufw')" == "" ]
 then
 	echo -e "[+] Install ufw "
 	apt install -y ufw > /dev/null 2>&1
