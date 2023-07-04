41c00fd29dc0cf7f104faa9e9ab9142e83b969a6
nnweg <119164122+nnweg@users.noreply.github.com>
2023-07-04 17:25:56 +0200

    Update 079_patch_sudoers_file.sh

--- global/level1/079_patch_sudoers_file.sh
+++ global/level1/079_patch_sudoers_file.sh
@@ -6,8 +6,8 @@ command=$(cat /etc/sudoers)
 if ! echo $command | grep -q 'Defaults' | grep -q 'use_pty'
 then
 	echo -e "[+] Adding 'Defaults use_pty' to /etc/sudoers"
- 	mkdir /etc/sudoers.d
-  	touch /etc/sudoers.d/cis_auto_rules
-   	echo "#includedir /etc/sudoers.d" >> /etc/sudoers
-	echo "Defaults use_pty" >> /etc/sudoers.d/cis_auto_rules
+ 	mkdir /etc/sudoers.d > /dev/null 2>&1
+  	touch /etc/sudoers.d/cis_auto_rules > /dev/null 2>&1
+   	echo "#includedir /etc/sudoers.d" >> /etc/sudoers > /dev/null 2>&1
+	echo "Defaults use_pty" >> /etc/sudoers.d/cis_auto_rules > /dev/null 2>&1
 fi
