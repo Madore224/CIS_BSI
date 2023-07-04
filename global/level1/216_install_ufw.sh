7d064224fea64345a3d4965bb87e7deede60bd35
Madore224 <112936472+Madore224@users.noreply.github.com>
2023-07-04 17:27:25 +0200

    Update 216_install_ufw.sh

--- global/level1/216_install_ufw.sh
+++ global/level1/216_install_ufw.sh
@@ -5,6 +5,6 @@
 #echo -e "[+] Check if ufw is installed "
 #if [ "$(dpkg-query -l | grep 'ufw')" == "" ]
 #then
-	echo -e "[+] Install ufw "
-	apt install -y ufw > /dev/null 2>&1
+echo -e "[+] Install ufw "
+apt install -y ufw > /dev/null 2>&1
 #fi
